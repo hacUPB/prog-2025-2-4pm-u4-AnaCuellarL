565fbf82caa127a75126c773f19f18c52fa83b4e
Ana Sofía Cuéllar Lozano <ana.cuellarl@upb.edu.co>
2025-10-02 16:48:14 -0500

    Actividades sobre tuplas

---
+++ Tuplas/Actividad_3_tuplas.py
@@ -0,0 +1,47 @@
+'''
+# Tupla vacía
+coordenada = ()
+
+# Tupla con elementos
+coordenada = (33.9425, -118.4081)  # LAX (Aeropuerto de Los Ángeles)
+
+coordenada[0] = 43.9
+print(coordenada) Esto es un error porque la tupla es INMUTABLE
+
+# Tupla con un solo elemento (requiere coma al final)
+rumbo = (270,)  # Sin la coma sería tratado como un entero entre paréntesis
+
+# Tupla sin paréntesis (empaquetado implícito)
+avion_info = "Boeing 787", "Dreamliner", 2009, 242
+
+# Indexación (similar a las listas)
+print(coordenada[0])  # 33.9425
+print(avion_info[-1])  # 242 (pasajeros)
+
+# Slicing
+print(avion_info[0:2])  # ("Boeing 787", "Dreamliner")
+
+# Desempaquetado de tuplas
+fabricante, modelo, año, capacidad = avion_info
+print(f"El {fabricante} {modelo} se lanzó en {año}")
+
+# Desempaquetado con *
+lat, lon = coordenada
+print(f"Latitud: {lat}, Longitud: {lon}")
+'''
+#lista_de_tuplas= [(0,0), (3,5), (8,3)]
+#print(lista_de_tuplas)
+#lista_de_tuplas.append((45,6))
+#print(lista_de_tuplas)
+#lista_de_tuplas[0]= (1,1)
+#print(lista_de_tuplas)
+'''
+lista_de_tuplas[1][0] = 1
+
+Esto no se puede hacer
+'''
+
+#tupla_de_listas = ([2,4,3], [9,6,12])
+#print(tupla_de_listas)
+#tupla_de_listas[1][0] =18
+#print(tupla_de_listas)
